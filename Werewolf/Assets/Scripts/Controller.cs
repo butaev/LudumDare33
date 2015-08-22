f740adbf9fa934a2d968c928cc5e2ee88c92e0e1
Vladimir <vl.ivakin@yandex.ru>
2015-08-22 12:40:56 +0600

    Add scripts

---
+++ Werewolf/Assets/Scripts/Controller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	private Transform cacheTransform
+	
+		private 
+
+	void Update () {
+		if (Math.abs (Input.GetAxis ("Horizonta;")) > 0.01f) {
+			tra
+		}
+	}
+	
+}
