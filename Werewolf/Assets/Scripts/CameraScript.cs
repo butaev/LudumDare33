8b2bda8a106d45d638319f01245a964d0f489f80
Vladimir <vl.ivakin@yandex.ru>
2015-08-24 15:05:07 +0600

    Add Clever Camera

---
+++ Werewolf/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	
+	public GameObject player;
+	private float y;
+	private float x;
+	public float hight;
+
+	void Awake (){
+		y = transform.position.y;
+		x = transform.position.x;
+	}
+	
+	void Update () {
+		if (player.transform.position.y < hight) {
+			transform.position = new Vector3 (player.transform.position.x + x, y, -10f); 
+		} else {
+			transform.position = new Vector3 (player.transform.position.x + x, (player.transform.position.y - hight) + y, -10f);
+		}
+	}
+}
--- Werewolf/Assets/Scripts/Controller.cs
+++ Werewolf/Assets/Scripts/Controller.cs
@@ -5,6 +5,7 @@ public class Controller : MonoBehaviour {
 
 	public float humanMovementSpeed = 10.0f;
 	public float werewolfMovementSpeed = 20.0f;
+	public float atackRadius;
 	private float timeJump = 0.5f;
 	private float timerJump;
 	private Transform cachedTransform;
@@ -13,54 +14,57 @@ public class Controller : MonoBehaviour {
 	private bool isGround = false;
 	private bool inShadow;
 	public bool human = true;
-
+	private float yScale;
+	private float xScale;
 	private bool hiding = false;
+	private float direction;
 
 	private void Awake() {
 		cachedTransform = GetComponent<Transform>();
 		cachedRigidbody = GetComponent<Rigidbody2D>();
 		cachedCollider = GetComponent<Collider2D>();
+		yScale = transform.localScale.y;
+		xScale = transform.localScale.x;
 	}
 
 	void OnTriggerStay2D(Collider2D trigger) {
-		if (human) {
-
-			if (Input.GetButtonDown ("Vertical") && !hiding && trigger.transform.GetComponent<Collider2D> ().tag == "Door" && Input.GetAxis("Vertical") > 0f) {
+		if (trigger.transform.GetComponent<Collider2D> ().tag == "Door") {
+			if (human && Input.GetButtonDown ("Vertical") && !hiding && Input.GetAxis("Vertical") > 0f) {
 				Debug.Log ("Test1");
-				cachedCollider.enabled = false;
 				cachedRigidbody.isKinematic = true;
+				cachedCollider.enabled = false;
+				gameObject.GetComponent<SpriteRenderer>().enabled = false;
 				hiding = true;
-				return;
-			}
-			if (Input.GetButtonDown ("Vertical") && hiding) {
-				Debug.Log ("Test2");
-				cachedRigidbody.isKinematic = false;
-				hiding = false;
-				return;
 			}
 		}
 	}
 
+	private void Atack(float direction) {
+		Physics2D.Raycast (cachedTransform.position, new Vector2 (direction, 0), atackRadius, 1 << LayerMask.NameToLayer ("Enemy"));
+	}
+
 	private void Update () {
 		human = GetComponent<Player_main_script> ().inShadow;
+		float horizontal = Input.GetAxis("Horizontal");
+		direction = Mathf.Sign (horizontal);
+		if (horizontal != 0){
+			transform.localScale = new Vector2(direction * xScale, yScale);
+		}
 		if (human) {
-			float horizontal = Input.GetAxis ("Horizontal");
-
-			if (hiding && Input.GetButtonDown ("Vertical")) {
-				horizontal = 0f;
-			}
-			cachedRigidbody.velocity = new Vector2 (horizontal * humanMovementSpeed, cachedRigidbody.velocity.y);
-
-			if (Input.GetButtonDown ("Horizontal") && hiding) {
-				horizontal = Input.GetAxis ("Horizontal");
-				Debug.Log ("Test3");
-				cachedCollider.enabled = true;
-				cachedRigidbody.isKinematic = false;
+			if (hiding){
+				if (Input.GetButtonDown ("Vertical") && Input.GetAxis("Vertical") < 0f){
+					cachedCollider.enabled = true;
+					cachedRigidbody.isKinematic = false;
+					gameObject.GetComponent<SpriteRenderer>().enabled = true;
+					hiding = false;
+				}
+			}else{
+				cachedRigidbody.velocity = new Vector2 (horizontal * humanMovementSpeed, cachedRigidbody.velocity.y);
 			}
 		} else {
-			float horizontal = Input.GetAxis("Horizontal");
 			cachedRigidbody.velocity = new Vector2( horizontal * werewolfMovementSpeed, cachedRigidbody.velocity.y);
 			isGround = (Physics2D.Raycast (cachedTransform.position, Vector2.down, 6f, 1 << LayerMask.NameToLayer ("Ground")) || Physics2D.Raycast (cachedTransform.position, Vector2.down, 6f, 1 << LayerMask.NameToLayer ("Box")));
+			Debug.Log(Physics2D.Raycast (cachedTransform.position, Vector2.down, 6f, 1 << LayerMask.NameToLayer ("Ground")));
 			if (Input.GetButtonDown ("Vertical") && isGround && timerJump >= timeJump && Input.GetAxis("Vertical") > 0f) {
 				cachedRigidbody.AddForce (Vector2.up  * 100000.0f);
 				timerJump = 0.0f;
