161ce93cd079514303b1446a37d6e989564879bb
butaev <butaev-raul@mail.ru>
2015-08-24 16:36:35 +0600

    add TestMenu

--- Werewolf/Assets/Scripts/Menu.cs
+++ Werewolf/Assets/Scripts/Menu.cs
@@ -3,13 +3,7 @@ using System.Collections;
 
 public class Menu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void PushStartButton () {
+		Application.LoadLevel("test");
 	}
 }
---
+++ Werewolf/Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMusic : MonoBehaviour {
+
+	public AudioClip track;
+	
+
+	private AudioSource source;
+
+	private void Start () {
+		source = GetComponent<AudioSource>();
+	}
+
+	public void PushMusicButton () {
+		source.clip = track;
+		source.Play ();
+	}
+}
