b50baec1ba87b11d46435992a460e983b6293199
Vladimir <vl.ivakin@yandex.ru>
2015-08-24 16:29:48 +0600

    Fix bug

--- Werewolf/Assets/Scripts/AI/WayPointSystem.cs
+++ Werewolf/Assets/Scripts/AI/WayPointSystem.cs
@@ -7,33 +7,21 @@ public class WayPointSystem : MonoBehaviour {
 	public GameObject point2;
 
 	private bool right = true;
-<<<<<<< HEAD
-=======
-	private Vector2 v1, v2;
->>>>>>> parent of 981897e... Add Footman Atack
+
 	private float v_left, v_right, dir;
 	private Transform cachedTransform;
 	private Rigidbody2D cachedRigidbody;
 
 	private void Start () {
 		cachedTransform = transform.parent.transform;
-<<<<<<< HEAD
-=======
-		v1 = point1.transform.position;
-		v2 = point2.transform.position;
->>>>>>> parent of 981897e... Add Footman Atack
+
 		v_left = point1.transform.position.x;
 		v_right = point2.transform.position.x;
 		if (v_left > v_right) {
 			v_left = v_right;
-<<<<<<< HEAD
 			v_right = point1.transform.position.x;
 			v_right = point1.transform.position.x;
-=======
-			v1 = v2;
-			v_right = point1.transform.position.x;
-			v2 = point1.transform.position;
->>>>>>> parent of 981897e... Add Footman Atack
+
 		}
 		dir = v_right;
 		Destroy (point1);
